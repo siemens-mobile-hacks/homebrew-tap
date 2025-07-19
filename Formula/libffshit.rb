a4231b8159e62bda5d9aa7ad430891f22def972e
Roman Serov <roman@serov.co>
2025-07-19 22:51:37 +0300

    libffshit, ffnightman

---
+++ Formula/libffshit.rb
@@ -0,0 +1,22 @@
+class Libffshit < Formula
+  version "latest"
+  sha256 :no_check
+  desc "Siemens Mobile phones filesystem library"
+  homepage "https://github.com/siemens-mobile-hacks/libffshit"
+  url "https://github.com/siemens-mobile-hacks/libffshit.git", branch: "main"
+  license "MIT"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on "cmake" => :build
+  depends_on "fmt" => :build
+
+  def install
+    system "cmake", "-DCMAKE_BUILD_TYPE=Release", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build", "--config", "Release"
+    system "cmake", "--install", "build", "--config", "Release"
+  end
+end
